378f85421b2c2bdd7610a0f0a27de59c9bd99d4b
Samat Tulegenov <magnit56@gmail.com>
2021-09-01 13:20:00 +0500

    Добавлена версия nodejs в package.json

--- public/js/app.js
+++ public/js/app.js
@@ -19445,7 +19445,7 @@ process.umask = function() { return 0; };
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkmyapp"] = self["webpackChunkmyapp"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
