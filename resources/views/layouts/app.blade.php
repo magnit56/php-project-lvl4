cf0d6fe257a8e2f2fa19fe523a2c07bc2a80337e
Samat Tulegenov <magnit56@gmail.com>
2021-09-08 11:54:33 +0500

    Добавлено отображение активной страницы

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,13 +35,13 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="/tasks">Задачи</a>
+                            <a class="nav-link {{ request()->is('tasks') ? 'active' : '' }}" href="/tasks">Задачи</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/task_statuses">Статусы</a>
+                            <a class="nav-link {{ request()->is('task_statuses') ? 'active' : '' }}" href="/task_statuses">Статусы</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link " href="/labels">Метки</a>
+                            <a class="nav-link {{ request()->is('labels') ? 'active' : '' }}" href="/labels">Метки</a>
                         </li>
                     </ul>
 
