6f8ed8c7ea668047be847e26f64760909fe301c2
Samat Tulegenov <magnit56@gmail.com>
2021-09-08 12:01:10 +0500

    Добавлен выброс статуса 403 в случае непрохождения авторизации

--- app/Http/Controllers/TaskStatusController.php
+++ app/Http/Controllers/TaskStatusController.php
@@ -15,20 +15,31 @@ class TaskStatusController extends Controller
         return view('taskStatus.index', compact('taskStatuses'));
     }
 
-    public function show($id)
+    public function show(Request $request, $id)
     {
         $taskStatus = TaskStatus::findOrFail($id);
+        if (optional($request->user())->cannot('view', $taskStatus)) {
+            abort(403);
+        }
+
         return view('taskStatus.show', compact('taskStatus'));
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        if (optional($request->user())->cannot('create', TaskStatus::class)) {
+            abort(403);
+        }
         $taskStatus = new TaskStatus();
         return view('taskStatus.create', compact('taskStatus'));
     }
 
     public function store(Request $request)
     {
+        if (optional($request->user())->cannot('create', TaskStatus::class)) {
+            abort(403);
+        }
+
         $data = ['name' => $request->input('name')];
         $validator = Validator::make($data, [
             'name' => 'required|unique:App\Models\TaskStatus',
@@ -50,26 +61,36 @@ class TaskStatusController extends Controller
             ->route('taskStatus.index');
     }
 
-    public function destroy($id)
+    public function destroy(Request $request, $id)
     {
         // Добавить то, что удалять статусы, связанные с задачами нельзя!
-        $taskStatus = TaskStatus::find($id);
-        if ($taskStatus) {
-            $taskStatus->delete();
+        $taskStatus = TaskStatus::findOrFail($id);
+        if (optional($request->user())->cannot('delete', $taskStatus)) {
+            abort(403);
         }
+
+        $taskStatus->delete();
         flash(trans('flash.status.deleted'))->success();
         return redirect()->route('taskStatus.index');
     }
 
-    public function edit($id)
+    public function edit(Request $request, $id)
     {
         $taskStatus = TaskStatus::findOrFail($id);
+        if (optional($request->user())->cannot('update', $taskStatus)) {
+            abort(403);
+        }
+
         return view('taskStatus.edit', compact('taskStatus'));
     }
 
     public function update(Request $request, $id)
     {
         $taskStatus = TaskStatus::findOrFail($id);
+        if (optional($request->user())->cannot('update', $taskStatus)) {
+            abort(403);
+        }
+
         $data = ['name' => $request->input('name')];
 
         $validator = Validator::make($data, [
