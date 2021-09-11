2b6e42cd68748beaab3f1d61cf1a31c73716bf6d
Samat Tulegenov <magnit56@gmail.com>
2021-09-11 14:37:19 +0500

    Добавлены связи между сущностями

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -15,4 +15,19 @@ class Task extends Model
         'status_id',
         'assigned_to_id',
     ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by_id');
+    }
+
+    public function assignee()
+    {
+        return $this->belongsTo(User::class, 'assigned_to_id');
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(TaskStatus::class, 'status_id');
+    }
 }
--- app/Models/TaskStatus.php
+++ app/Models/TaskStatus.php
@@ -12,4 +12,9 @@ class TaskStatus extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'status_id');
+    }
 }
