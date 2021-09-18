20df79a1577eb9a86c3ca7e0b7c1c74ebe08f978
Samat Tulegenov <magnit56@gmail.com>
2021-09-18 21:15:32 +0500

    Поправлены ошибки локализации входа

--- resources/lang/ru/auth.php
+++ resources/lang/ru/auth.php
@@ -17,7 +17,8 @@ return [
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'custom' => [
-        'login' => 'Логин',
+        'login' => 'Вход',
+        'sendLogin' => 'Войти',
         'email' => 'Email',
         'password' => 'Пароль',
         'remember' => 'Запомнить меня',
