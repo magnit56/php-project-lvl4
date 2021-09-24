401b614906c279ef69eae9fd3f38f61ee6a5e613
Samat Tulegenov <magnit56@gmail.com>
2021-09-25 04:52:11 +0500

    Удалено отправление сообщения на почту после регистрации

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        Mail::to($data['email'])->queue(new NewUserNotification());
+//        Mail::to($data['email'])->queue(new NewUserNotification());
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
